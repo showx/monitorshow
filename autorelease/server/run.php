b9984f3e7dc60e3421f67f789c224c1d38bcc156
show <showshou@126.com>
2019-10-15 11:24:25 +0800

    增加发布系统服务端调用

--- autorelease/server/run.php
+++ autorelease/server/run.php
@@ -13,9 +13,19 @@ if(isset($config[$project]))
     if($config[$project]['gitToken'] == $gitToken)
     {
         //ok的情况下执行
-        echo 'test';
+        echo 'ok';
+        //记录一下
+        $time = time();
+        $pro_argv1 = $config[$project]['gitName'];
+        $pro_argv2 = $config[$project]['webName'];
+        $shell = "bash /show/monitorshow/autorelease/gitsync.sh {$pro_argv1} {$pro_argv2}";
+        system($shell, $status);
+        $log = "project:{$project} status:{$status} success time:{$time} ";
+        file_put_contents("/show/monitorshow/autorelease/server/record.log",$log,FILE_APPEND|LOCK_EX);
     }else{
         echo 'token error';
+        $log = "project:{$project} error time:{$time} ";
+        file_put_contents("/show/monitorshow/autorelease/server/record.log",$log,FILE_APPEND|LOCK_EX);
     }
 }else{
     echo 'server error!';
